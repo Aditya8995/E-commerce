a1811ff778b0b5911133fc8d5990e6b4d9038424
aditya raj malik <armmallik89@gmail.com>
2025-03-14 22:03:19 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const productRoutes = require('./routes/productRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+const orderRoutes = require('./routes/orderRoutes.js');
+const uploadRoutes = require('./routes/uploadRoutes.js');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
+const cookieParser = require('cookie-parser');
+
+dotenv.config(); 
+
+connectDB();
+
+const app = express();
+
+// body parser middelware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+app.get('/api/config/paypal', (req, res) => {
+  res.json({ clientId: process.env.PAYPAL_CLIENT_ID }); 
+});
+
+// const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
